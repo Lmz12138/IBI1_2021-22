2729f0f7f6bcb5b944398d0d6bbda94f14f60fa7
unknown <hanzhang.21@intl.zju.edu.cn>
2022-03-21 15:25:10 +0800

    age_on_offspring_health.py

---
+++ Practical6/age_on_offspring_health.py
@@ -0,0 +1,10 @@
+import numpy as np
+paternal_age = [30, 35, 40, 45, 50, 55, 60, 65, 70, 75] #list the data
+chd = [1.03, 1.07, 1.11, 1.17, 1.23, 1.32, 1.42, 1.55, 1.72, 1.94]
+print(paternal_age)
+print(chd)
+import matplotlib.pyplot as plt# import matlab package
+x = paternal_age #set the axes
+y = chd
+plt.scatter(x, y, color='blue') #set the figure
+plt.show()
