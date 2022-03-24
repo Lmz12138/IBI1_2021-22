debd8c4079357cef7398f3531cc33a3430563e29
unknown <hanzhang.21@intl.zju.edu.cn>
2022-03-24 17:06:51 +0800

    age_on_offspring_health.py

--- Practical6/age_on_offspring_health.py
+++ Practical6/age_on_offspring_health.py
@@ -4,12 +4,12 @@ chd = [1.03, 1.07, 1.11, 1.17, 1.23, 1.32, 1.42, 1.55, 1.72, 1.94]
 print(paternal_age)
 print(chd)
 dic = {'30': 1.03, '35': 1.07, '40': 1.11, '45': 1.17, '50': 1.23, '55': 1.32,
-       '60': 1.42, '65': 1.55, '70': 1.72, '75': 1.94}
-print(dic)# make a dictionary
+       '60': 1.42, '65': 1.55, '70': 1.72, '75': 1.94}# make a dictionary
+print(dic)
 import matplotlib.pyplot as plt# import matlab package
 x = paternal_age #set the axes
 y = chd
 plt.scatter(x, y, color='blue') #set the figure
 plt.show()
-print(dic['45'])
+print(dic['45'])#Check for correctness
 print(dic['55'])
